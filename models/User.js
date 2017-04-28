5f28300c061921de021305f895c2237877b2a9b6
Rostislav <radeff@radeff.eu>
2017-04-28 17:58:53 +0300

    Added a date record to highscore
    Modifed User model to record Date for highScore.
    Added a column for Date Achieved in highscores view.

--- models/User.js
+++ models/User.js
@@ -8,6 +8,7 @@ let userSchema = mongoose.Schema({
         fullName: {type: String, required: true},
         articles: [{type: mongoose.Schema.Types.ObjectId, ref: 'Article'}],
         highScore: {type: Number, default: 0},
+        dateAchieved: {type: Date, default: Date.now()},
         roles: [{type: mongoose.Schema.Types.ObjectId, ref: 'Role'}],
         salt: {type: String, required: true}
     });
