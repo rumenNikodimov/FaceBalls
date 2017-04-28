698d1de0f8350fbb290a66eb58243d4b2d775869
Rostislav <radeff@radeff.eu>
2017-04-28 18:57:24 +0300

    Highscore saving logic
    Added logic to save highscore in database after game ends.

--- controllers/game.js
+++ controllers/game.js
@@ -1,3 +1,5 @@
+const User = require("mongoose").model("User");
+
 module.exports = {
     gameGet: (req, res) => {
         if(!req.isAuthenticated()) {
@@ -12,6 +14,16 @@ module.exports = {
             res.redirect('/user/login');
             return;
         }
+        res.redirect('/game');
+
+        let score = parseFloat(req.body.score);
+        User.findById(req.user.id).then(user => {
+            if(score > user.highScore) {
+                user.highScore = score;
+                user.dateAchieved = Date.now();
+                user.save();
+            }
+        });
         res.redirect('/game');
     }
 };
